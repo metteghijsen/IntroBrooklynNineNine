24dfd014e6b4158aa2e6cb388ab0b22984434ce3
Mette <mette.ghijsen@gmail.com>
2022-02-22 12:43:28 +0100

    gekleurde overlay, en popup character overlay toegevoegd. ook namen van acteurs en gekleurde achtergronden toegevoegd

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
 <section id="playScreen" style="display: none">
     <div class="container-text">
-        <div id="title" class="title">
+        <div id="title" class="title" style="display: none">
             <div class="brooklyn">
                 <h1>
                     BROOKLYN
@@ -34,10 +34,89 @@
             </div>
         </div>
 
+        <div class="character-description">
+            <div class="jake" id="jake" style="display: none">
+                <p>starring</p>
+                <h1>ANDY </h1>
+                <h1>SAMBERG</h1>
+            </div>
+
+            <div class="rosa" id="rosa" style="display: none">
+                <h1>STEPHANIE <br>
+                    BEATRIZ</h1>
+            </div>
+
+            <div class="terry" id="terry" style="display: none">
+                <h1>TERRY <br>
+                    CREWS</h1>
+            </div>
+
+            <div class="amy" id="amy" style="display: none">
+                <h1>MELISSA <br>
+                    FUMERO</h1>
+            </div>
+
+            <div class="charles" id="charles" style="display: none">
+                <h1>JOE <br>
+                    LO TRUGLIO</h1>
+            </div>
+
+            <div class="gina" id="gina" style="display: none">
+                <h1>CHELSEA <br>
+                    PERETTI</h1>
+            </div>
+
+            <div class="holt" id="holt" style="display: none">
+                <p>and</p>
+                <h1>ANDRE <br>
+                    BRAUGHTER</h1>
+            </div>
+
+            <div class="creator" id="creator" style="display: none">
+                <p>created by</p>
+                <h1>METTE <br>
+                    GHIJSEN</h1>
+            </div>
+        </div>
     </div>
 
-    <div class="afbeelding">
-        <div id="afbeelding1" class="afbeelding">
+    <div class="character-overlay">
+        <div id="jakeoverlay" class="jakeoverlay" style="display: none">
+            <img src="img/jakeoverlay.png">
+        </div>
+
+        <div id="rosaoverlay" class="rosaoverlay" style="display: none">
+            <img src="img/rosaoverlay.png">
+        </div>
+
+        <div id="terryoverlay" style="display: none">
+            <img src="img/terryoverlay1.png" class="terryoverlay">
+            <img src="img/terryoverlay2.png" class="terryoverlay2">
+        </div>
+
+        <div id="amyoverlay" class="amyoverlay" style="display: none">
+            <img src="img/amyoverlay.png">
+        </div>
+
+        <div id="charlesoverlay" class="charlesoverlay" style="display: none">
+            <img src="img/charlesoverlay.png"">
+        </div>
+
+        <div id="ginaoverlay" style="display: none">
+            <img src="img/ginaoverlay.png" class="ginaoverlay">
+            <img src="img/ginaoverlay2.png" class="ginaoverlay2">
+        </div>
+
+        <div id="holtoverlay" class="holtoverlay" style="display: none">
+            <img src="img/holtoverlay.png">
+        </div>
+    </div>
+
+    <div id="overlay">
+    </div>
+
+    <div class="afbeelding" id="afbeelding">
+        <div id="afbeelding1">
             <img src="img/afbeelding1.png">
         </div>
 
--- script.js
+++ script.js
@@ -4,50 +4,125 @@ function playIntro(){
     startScreen.style.display = "none";
     playScreen.style.display = "block";
 
-    music.play();
+    music.play(); //muziek gaat aan
 
-    setTimeout(function Titlescreen(){
+    setTimeout(function Titlescreen(){ //titelscherm komt in beeld
         document.getElementById('afbeelding1').style.display = "block";
+        document.getElementById('title').style.display = "flex";
     })
 
-    setTimeout(function Jake(){
+    setTimeout(function Jake(){ //jake komt in beeld
         document.getElementById('afbeelding1').style.display = "none";
+        document.getElementById('title').style.display = "none";
+
         document.getElementById('afbeelding2').style.display = "block";
+        setTimeout(function Overlay(){ //overlay met tekst
+            document.getElementById('overlay').style.display= "block";
+            document.getElementById('jake').style.display= "block";
+            document.getElementById('jakeoverlay').style.display = "block";
+        },1000)
     },2200);
 
-    setTimeout(function Rosa(){
+    setTimeout(function Rosa(){ //jake uit beeld, rosa in beeld
         document.getElementById('afbeelding2').style.display = "none";
+        document.getElementById('overlay').style.display= "none";
+        document.getElementById('jake').style.display= "none";
+        document.getElementById('jakeoverlay').style.display = "none";
+
         document.getElementById('afbeelding3').style.display = "block";
+        setTimeout(function Overlay(){ //overlay met tekst
+            document.getElementById('overlay').style.display= "block";
+            document.getElementById('overlay').style.backgroundColor = "rgba(0,123,245,0.5)";
+            document.getElementById('rosa').style.display= "block";
+            document.getElementById('rosaoverlay').style.display = "block";
+        },1000)
     }, 4400)
 
-    setTimeout(function Terry(){
+    setTimeout(function Terry(){ //rosa uit beeld, terry in beeld
         document.getElementById('afbeelding3').style.display = "none";
+        document.getElementById('overlay').style.display= "none";
+        document.getElementById('rosa').style.display= "none";
+        document.getElementById('rosaoverlay').style.display = "none";
+
         document.getElementById('afbeelding4').style.display = "block";
+        setTimeout(function Overlay(){ //overlay met tekst
+            document.getElementById('overlay').style.display= "block";
+            document.getElementById('overlay').style.backgroundColor = "rgba(245,82,0,0.5)";
+            document.getElementById('terry').style.display= "block";
+            document.getElementById('terryoverlay').style.display = "block";
+        },1000)
     }, 6600)
 
-    setTimeout(function Amy(){
+    setTimeout(function Amy(){ //terry uit beeld, amy in beeld
         document.getElementById('afbeelding4').style.display = "none";
+        document.getElementById('overlay').style.display= "none";
+        document.getElementById('terry').style.display= "none";
+        document.getElementById('terryoverlay').style.display = "none";
+
         document.getElementById('afbeelding5').style.display = "block";
+        setTimeout(function Overlay(){ //overlay met tekst
+            document.getElementById('overlay').style.display= "block";
+            document.getElementById('overlay').style.backgroundColor = "rgba(245,208,0,0.5)";
+            document.getElementById('amy').style.display= "block";
+            document.getElementById('amyoverlay').style.display = "block";
+        },1000)
     }, 8800)
 
-    setTimeout(function Charles(){
+    setTimeout(function Charles(){ //amy uit beeld, charles in beeld
         document.getElementById('afbeelding5').style.display = "none";
+        document.getElementById('overlay').style.display= "none";
+        document.getElementById('amy').style.display= "none";
+        document.getElementById('amyoverlay').style.display = "none";
+
         document.getElementById('afbeelding6').style.display = "block";
+        setTimeout(function Overlay(){ //overlay met tekst
+            document.getElementById('overlay').style.display= "block";
+            document.getElementById('overlay').style.backgroundColor = "rgba(255,0,0,0.5)";
+            document.getElementById('charles').style.display= "block";
+            document.getElementById('charlesoverlay').style.display = "block";
+        },1000)
     }, 11000)
 
-    setTimeout(function Gina(){
+    setTimeout(function Gina(){ //charles uit beeld, gina in beeld
         document.getElementById('afbeelding6').style.display = "none";
+        document.getElementById('overlay').style.display= "none";
+        document.getElementById('charles').style.display= "none";
+        document.getElementById('charlesoverlay').style.display = "none";
+
         document.getElementById('afbeelding7').style.display = "block";
+        setTimeout(function Overlay(){ //overlay met tekst
+            document.getElementById('overlay').style.display= "block";
+            document.getElementById('overlay').style.backgroundColor = "rgba(0,123,245,0.5)";
+            document.getElementById('gina').style.display= "block";
+            document.getElementById('ginaoverlay').style.display = "block";
+        },1000)
     }, 13200)
 
-    setTimeout(function Holt(){
+    setTimeout(function Holt(){ //gina uit beeld, holt in beeld
         document.getElementById('afbeelding7').style.display = "none";
+        document.getElementById('overlay').style.display= "none";
+        document.getElementById('gina').style.display= "none";
+        document.getElementById('ginaoverlay').style.display = "none";
+
         document.getElementById('afbeelding8').style.display = "block";
+        setTimeout(function Overlay(){ //overlay met tekst
+            document.getElementById('overlay').style.display= "block";
+            document.getElementById('overlay').style.backgroundColor = "rgba(245,82,0,0.5)";
+            document.getElementById('holt').style.display= "block";
+            document.getElementById('holtoverlay').style.display = "block";
+        },1000)
     }, 15400)
 
-    setTimeout(function Author(){
+    setTimeout(function Author(){ //holt uit beeld, credits in beeld
         document.getElementById('afbeelding8').style.display = "none";
+        document.getElementById('overlay').style.display= "none";
+        document.getElementById('holt').style.display= "none";
+        document.getElementById('holtoverlay').style.display = "none";
+
         document.getElementById('afbeelding9').style.display = "block";
+        setTimeout(function Credits(){
+            document.getElementById('creator').style.display= "block";
+        },1000)
     }, 17600)
 }
 
